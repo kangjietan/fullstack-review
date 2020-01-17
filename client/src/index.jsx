89514b989233f8aa979b6d92e955a953c49cb7e9
Kang Jie Tan <kangjietan@gmail.com>
2020-01-17 15:54:16 -0800

    Allow client to search user's repos

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,21 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+import axios from 'axios';
 import Search from './components/Search.jsx';
 import RepoList from './components/RepoList.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       repos: []
     }
 
   }
 
+  // ComponentDidMount for GET requests to display top 25
+  // Ajax GET
+
+  // Ajax POST request to localhost/repos
   search (term) {
     console.log(`${term} was searched`);
     // TODO
+    // $.post("http://localhost:1128/repos", {term})
+    //   .done(function(data) {
+    //     console.log('Success', data);
+    //   });
+    axios.post('http://localhost:1128/repos', {term})
+      .then(function(response) {
+        console.log(response);
+        // console.log(Array.isArray(response));
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
   }
 
   render () {
