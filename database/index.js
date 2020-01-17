09f5d24e7fe83b9b2e783c25d7623a9c9f117ede
Kang Jie Tan <kangjietan@gmail.com>
2020-01-17 11:53:51 -0800

    Define schema for github API data

--- database/index.js
+++ database/index.js
@@ -1,8 +1,20 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
+// Data = [{..}, {..}, {..}, .....]
+// id: Number
+// full_name: String
+// link: String -> owner.html_url: (which is the link)
+// popularity: Number -> stargazers_count (repo 'followers')
+// Sort by stargazers_count for top 25
+// Uniqueness will be based on id
+
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+  id: Number,
+  full_name: String,
+  link: String,
+  popularity: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
