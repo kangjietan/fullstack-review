1595ef9bc6d95415d7f27fed328e80e71848c00a
Kang Jie Tan <kangjietan@gmail.com>
2020-01-17 15:51:32 -0800

    Make GET request to github API on client post request

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const github = require('../helpers/github.js');
+const db = require('../database/index.js')
 let app = express();
-
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../client/dist'));
 
 app.post('/repos', function (req, res) {
@@ -8,6 +12,12 @@ app.post('/repos', function (req, res) {
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
+
+  var callback = function(data) {
+    console.log(data);
+    res.end(JSON.stringify(data));
+  }
+  github.getReposByUsername(req.body.term, callback);
 });
 
 app.get('/repos', function (req, res) {
