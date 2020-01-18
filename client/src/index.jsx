e21153bfa3b41fda302454e52b194d916afe55d0
Kang Jie Tan <kangjietan@gmail.com>
2020-01-17 20:40:56 -0800

    On app load, make GET request to server for top 25
    When POST request is sent with username, display the repos
    from Github API

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -8,27 +8,43 @@ import RepoList from './components/RepoList.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       repos: []
     }
 
+    this.testFun = this.testFun.bind(this);
   }
 
   // ComponentDidMount for GET requests to display top 25
   // Ajax GET
+  componentDidMount() {
+    axios.get('http://localhost:1128/repos')
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+
+  testFun (data) {
+    this.setState({
+      repos: data
+    })
+  }
 
   // Ajax POST request to localhost/repos
   search (term) {
     console.log(`${term} was searched`);
-    // TODO
-    // $.post("http://localhost:1128/repos", {term})
-    //   .done(function(data) {
-    //     console.log('Success', data);
-    //   });
+    var postData;
+    var testFun = this.testFun;
     axios.post('http://localhost:1128/repos', {term})
       .then(function(response) {
-        console.log(response);
+        // console.log(Array.isArray(response.data));
         // console.log(Array.isArray(response));
+        postData = response.data;
+        testFun(postData);
       })
       .catch(function(error) {
         console.log(error);
