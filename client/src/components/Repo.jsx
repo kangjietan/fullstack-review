23b5a0edec0eccb67b7d8b38c759b2efa5b391e5
Kang Jie Tan <kangjietan@gmail.com>
2020-01-17 23:45:16 -0800

    Render repo properties to App

--- client/src/components/Repo.jsx
+++ client/src/components/Repo.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
+// id: repo.id,
+// name: repo.name,
+// link: repo.owner.html_url,
+// popularity: repo.stargazers_count
+
 var Repo = (props) => {
   return (
-    <div>
-
-    </div>
+    <tr>
+      <td>{props.repo.id}</td>
+      <td>{props.repo.name}</td>
+      <td>{props.repo.link}</td>
+      <td>{props.repo.popularity}</td>
+    </tr>
   );
 }
+
+export default Repo;
