debe347abf29e4f1a8bd624d0b9e5edb4b4e1803
Kang Jie Tan <kangjietan@gmail.com>
2020-01-17 18:26:35 -0800

    Splice duplicate entry out of array before saving to db

--- database/index.js
+++ database/index.js
@@ -23,21 +23,77 @@ let save = (params, cb) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
-  var repo = new Repo({
-    id: 1,
-    name: 'Test',
-    link: 'Test123',
-    popularity: 0
-  });
 
-  repo.save(function(err) {
+  // Splice the entry out of the array if it exists inside the db
+  for (var i = 0; i < params.length; i++) {
+    if (Repo.find(params[i].id)) {
+      params.splice(i, 1);
+      i--;
+    }
+  }
+  console.log('Params array', params);
+
+  Repo.insertMany(params, function(err, docs) {
     if (err) {
       console.log(err);
     } else {
-      console.log('Success')
+      console.log('Insert multiple elements/repos', docs);
     }
     cb();
-  })
+  });
+
+  // var newRepo = {
+  //   id: 1,
+  //   name: 'Test',
+  //   link: 'Test123',
+  //   popularity: 0
+  // }
+
+  // var repo = new Repo({
+  //   id: 1,
+  //   name: 'Test',
+  //   link: 'Test123',
+  //   popularity: 0
+  // });
+
+  // repo.save(function(err) {
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //     console.log('Success')
+  //   }
+  //   cb();
+  // });
+
+  // var arr = [
+  //   {
+  //     id: 1,
+  //     name: 'Test',
+  //     link: 'Test1234',
+  //     popularity: 0
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'Test1',
+  //     link: 'Test1235',
+  //     popularity: 1
+  //   },
+  //   {
+  //     id: 3,
+  //     name: 'Test2',
+  //     link: 'Test1236',
+  //     popularity: 2
+  //   }
+  // ]
+
+  // Repo.insertMany(arr, function(err, docs) {
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //     console.log('Insert multiple elements/repos');
+  //   }
+  //   cb();
+  // });
 }
 
 module.exports.save = save;
