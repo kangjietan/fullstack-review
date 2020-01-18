f2fcf8be4b5e1b9795b9efcf0c0322c943d99356
Kang Jie Tan <kangjietan@gmail.com>
2020-01-18 12:33:38 -0800

    Add <tbody>

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -6,13 +6,15 @@ const RepoList = (props) => (
     <h4> Repo List Component </h4>
     There are {props.repos.length} repos.
     <table>
-      <tr>
-        <td>ID</td>
-        <td>Name</td>
-        <td>UserLink</td>
-        <td>Stars</td>
-      </tr>
-      {props.repos.map(repo => <Repo repo={repo}/>)}
+      <tbody>
+        <tr>
+          <td>ID</td>
+          <td>Name</td>
+          <td>UserLink</td>
+          <td>Stars</td>
+        </tr>
+        {props.repos.map(repo => <Repo repo={repo}/>)}
+      </tbody>
     </table>
   </div>
 )
