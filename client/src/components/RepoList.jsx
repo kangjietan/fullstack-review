c116b7c4dccafb2fe54c0f4859a7b846ce4097a1
Kang Jie Tan <kangjietan@gmail.com>
2020-01-17 23:45:43 -0800

    Set up table to display repo info

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import Repo from './Repo.jsx';
 
 const RepoList = (props) => (
   <div>
     <h4> Repo List Component </h4>
     There are {props.repos.length} repos.
+    <table>
+      <tr>
+        <td>ID</td>
+        <td>Name</td>
+        <td>Link</td>
+        <td>Stars</td>
+      </tr>
+      {props.repos.map(repo => <Repo repo={repo}/>)}
+    </table>
   </div>
 )
 
